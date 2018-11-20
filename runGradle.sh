4d61fe075f2a906d0a23931dacd517b1acf88840
Purushotham Reddy <purushothamreddy@Purushothams-MacBook-Pro.local>
2018-11-20 19:01:07 +0530

    chnages to docker compose file

--- runGradle.sh
+++ runGradle.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gradle clean build
+gradle Sanity
--- wait-for-grid.sh
+++ wait-for-grid.sh
@@ -2,14 +2,13 @@
 
 set -e
 
-cmd="$@"
-
 while ! curl -sSL "http://localhost:4444/wd/hub/status" 2>&1 \
-        | jq -r '.value.ready' 2>&1 | grep "true" >/dev/null; do
+        | jq -r '.value.ready' 2>&1 | grep "true"; do
     echo 'Waiting for the Grid'
     sleep 1
 done
 
+
 >&2 echo "Selenium Grid is up - executing test"
 
-exec $cmd
+exec /bin/sh ./runGradle.sh
